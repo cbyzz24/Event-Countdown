1c8d0f5e800ccdc356d303faeddcedec7f30b8aa
cbyzz24 <155403160+cbyzz24@users.noreply.github.com>
2025-05-10 17:25:02 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function updateTimeCount(){
+    let currentDate = Date.parse(new Date());
+    let eventDate = Date.parse(new Date("May, 11 2025 13:30:00"));
+    let timeGap = eventDate - currentDate;
+    const dayEl = document.getElementById("dayCount");
+    const hourEl = document.getElementById("hourCount");
+    const minuteEl = document.getElementById("minuteCount");
+    const secondEl = document.getElementById("secondCount");
+
+    dayEl.value = Math.floor(timeGap / (1000 * 60 * 60 * 24));
+    hourEl.value = Math.floor(timeGap / (1000 * 60 * 60)%24);
+    minuteEl.value = Math.floor(timeGap / (1000 * 60) % 60);
+    secondEl.value = Math.floor(timeGap / (1000) % 60);
+}
+
+setInterval(updateTimeCount, 1000);
