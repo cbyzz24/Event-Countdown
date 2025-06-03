22d067d5351d6c2fc17499c8dcb232a48058ef24
cbyzz24 <155403160+cbyzz24@users.noreply.github.com>
2025-06-02 19:14:07 -0600

    Enhanced JS syntax - Improved time display responsive sizing - Updated countdown description label

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
+const daysEl = document.getElementById("dayCount");
+const hoursEl = document.getElementById("hourCount");
+const minutesEl = document.getElementById("minuteCount");
+const secondsEl = document.getElementById("secondCount");
 
-function updateTimeCount(){
-    let currentDate = Date.parse(new Date());
-    let eventDate = Date.parse(new Date("August, 29 2025 00:00:00"));
-    let timeGap = eventDate - currentDate;
-    const dayEl = document.getElementById("dayCount");
-    const hourEl = document.getElementById("hourCount");
-    const minuteEl = document.getElementById("minuteCount");
-    const secondEl = document.getElementById("secondCount");
+function displayCountdown(){
+    let currentDay = new Date();
+    let eventDay = new Date("August 29, 2025 00:00:00");
+    let gap = eventDay - currentDay;
+    daysEl.value = Math.floor((gap/(1000 * 60 * 60 * 24)));
+    hoursEl.value = Math.floor((gap/(1000 * 60 * 60))%24);
+    minutesEl.value = Math.floor((gap/(1000 * 60))%60);
+    secondsEl.value = Math.floor((gap/(1000))%60);
 
-    dayEl.value = Math.floor(timeGap / (1000 * 60 * 60 * 24));
-    hourEl.value = Math.floor(timeGap / (1000 * 60 * 60)%24);
-    minuteEl.value = Math.floor(timeGap / (1000 * 60) % 60);
-    secondEl.value = Math.floor(timeGap / (1000) % 60);
 }
-
-setInterval(updateTimeCount, 1000);
+setInterval(displayCountdown, 1000);
+displayCountdown();
